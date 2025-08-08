a5580f8c7c7d9bcb06c569f74a7bd38f5685238e
cuong.duc.bui <cuong.duc.bui@advesa.com>
2025-08-08 17:33:13 +0700

    Init

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/' : '/bk/',
+  base: process.env.NODE_ENV === 'production' ? '/lmk/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
